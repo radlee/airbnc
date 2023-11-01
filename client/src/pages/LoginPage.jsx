1f39129b713d6b0636c9d66ef3d83be80a7f660c
lolito <prof.lolito@gmail.com>
2023-11-01 16:24:48 +0200

    Centered the Login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,12 +1,14 @@
 export default function LoginPage() {
     return(
-        <div className="mt-4">
-            <h1 className="text-4xl text-center">Logina</h1>
-            <form className="max-w-md mx-auto">
-                <input type="email" name="" placeholder="yourname@mail.com" />
-                <input type="password" name="" placeholder="password" />
-                <button className="primary">Logina</button>
-            </form>
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-32">
+                <h1 className="text-4xl text-center mb-4">Logina</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="email" name="" placeholder="yourname@mail.com" />
+                    <input type="password" name="" placeholder="password" />
+                    <button className="primary">Logina</button>
+                </form>
+            </div>
         </div>
     )
 }
