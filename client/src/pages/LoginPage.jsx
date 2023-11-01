c90555d03c8e1d7864c4392cac63a20072d229e7
lolito <prof.lolito@gmail.com>
2023-11-01 15:32:13 +0200

    Basic Setup

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+    return(
+        <div className="mt-4">
+            <form action="">
+                <input type="email" name="" placeholder="yourname@mail.com" />
+                <input type="password" name="" placeholder="password" />
+                <button>Logina</button>
+            </form>
+        </div>
+    )
+}
