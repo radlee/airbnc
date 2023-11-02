7aff01952feaa805f8ae2c925f00e29c862bf7a2
lolito <prof.lolito@gmail.com>
2023-11-02 07:42:49 +0200

    Registration completed. Loging in now

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -8,13 +8,20 @@ export default function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    function registerUser(ev) {
+    async function registerUser(ev) {
         ev.preventDefault();
-        axios.post('/register', {
-            name,
-            email,
-            password,
-        });
+        try {
+            await axios.post('/register', {
+                name,
+                email,
+                password,
+            });
+    
+            alert('Registered. Now you may login.')
+        } catch (error) {
+            alert("Registration failed. Please try again later.")
+        }
+        
     }
     return(
         <div className="mt-4 grow flex items-center justify-around">
